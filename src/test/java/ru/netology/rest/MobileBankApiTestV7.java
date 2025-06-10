4c58576b989edc421bd921d387e9305198346f29
abobus228 <sashasafronov767@gmail.com>
2025-06-10 21:39:48 +0300

    add test-class

---
+++ src/test/java/ru/netology/rest/MobileBankApiTestV7.java
@@ -0,0 +1,30 @@
+package ru.netology.rest;
+
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.equalToIgnoringCase;
+
+public class MobileBankApiTestV7 {
+
+    @Test
+    void shouldReturnDemoAccounts() {
+        // Given - When - Then
+        // Предусловия
+        given()
+                .baseUri("http://localhost:9999/api/v1")
+                // Выполняемые действия
+                .when()
+                .get("/demo/accounts")
+                // Проверки
+                .then()
+                .statusCode(200)
+                // специализированные проверки - лучше
+                .body("[0].currency", equalToIgnoringCase("rub"))
+                .body("[1].currency", equalToIgnoringCase("usd"))
+                .body("[2].currency", equalToIgnoringCase("rub"))
+        ;
+    }
+}
